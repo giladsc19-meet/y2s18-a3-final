590a9a6514c899e7802d925866a7a72a66119777
malak18-meet <malak.jabarin@gmail.com>
2018-08-12 12:39:41 +0300

    s

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask import Flask, render_template, url_for,request,redirect
 app = Flask(__name__)
 
 @app.route('/')
-<<<<<<< HEAD
+
+
+# this is for the login page
 def login():
     return render_template('login.html', users=query_all())
 @app.route('/add', methods=['GET', 'POST'])
@@ -18,7 +20,14 @@ def add_student_route():
 
 		return render_template('add.html')
 
-	
+# this is for the home page (feed page)
+
+@app.route('/home/user_name=<String:user_name> password=<int:password>')
+def home():
+    return render_template('home.html')
+
+
+
 @app.route('/user/<int:user_name>')
 def display_user(user_name):
     return render_template('login.html', user=query_by_user_name(user_name))
@@ -29,16 +38,3 @@ def delete(user_namr):
 		
 
 app.run(debug=True)
-=======
-def signUp():
-    return render_template('login.html')
-
-@app.route('/home/user_name=<String:user_name> password=<int:password>')
-def home():
-    return render_template('home.html')
-
-
-# Running the Flask app
-if __name__ == "__main__":
-    app.run(debug=True)
->>>>>>> 576060a3af181bdd55038c5278386fc0efcdc97f
