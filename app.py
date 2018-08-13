c6afe3a58c8d636126720cf13e1efff423fd7e69
malak18-meet <malak.jabarin@gmail.com>
2018-08-13 10:52:13 +0300

    s

--- app.py
+++ app.py
@@ -2,27 +2,25 @@ from databases import *
 from flask import Flask, render_template, url_for,request,redirect
 app = Flask(__name__)
 
-@app.route('/')
+########################################################################################## Dont care about the above !
 
 
-# this is for the login page
-
-@app.route('/add', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
+
 	if request.method == 'GET':
-		return render_template('login.html', users=get_all_users())
-	else:
-		
-		user_name= request.form['username']
+		return render_template('login.html')
+
+	if request.method == 'POST':
+		user_name= request.form['user_name']
 		password= request.form['password']
-		add_user(user_name,password)
-		return render_template('home.html')
+		status = add_user(user_name,password)
+		
+		if status:
+			return render_template('home.html', user = new_user)
+		else:
+			return render_template('login.html')
 
-# this is for the home page (feed page)
-
-@app.route('/home/<string:user_name>/<string:password>')
-def home(user_name, password):
-    return render_template('home.html', user_name = user_name, password = password)
 
 # this is for the profile page
 
@@ -30,12 +28,6 @@ def home(user_name, password):
 def display_user(user_name):
     return render_template('login.html', user=get_by_user_name(user_name))
 
-# this is for the 
 
-@app.route('/delete/<string:user_name>')
-def delete(user_name):
-    delete_user(user_name)
-    return redirect(url_for('login.html'))
-		
 if __name__ == "__main__":
     app.run(debug=True)
