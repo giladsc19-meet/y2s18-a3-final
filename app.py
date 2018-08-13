57ec03d3e2e91570dd29cd8e92288e72014b7a5f
aboud20-meet <aboud20@meet.mit.edu>
2018-08-13 14:21:14 +0300

    dd

--- app.py
+++ app.py
@@ -47,5 +47,11 @@ def display_user(user_name):
     return render_template('login.html', user=get_by_user_name(user_name))
 
 
+@app.route('/yyy')
+def dis():
+	return render_template('home.html')
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
