472e917fd3b9dae9c473387048e5c98fa4e09048
aboud20-meet <aboud20@meet.mit.edu>
2018-08-15 16:35:01 +0300

    jjj

--- app.py
+++ app.py
@@ -101,6 +101,9 @@ def contact():
 def about():
 	return render_template('whower.html')
 
+@app.route('/help')
+def help():
+	return render_template('help.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)
